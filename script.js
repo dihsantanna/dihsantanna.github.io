62091a6acb2a286ce2755e584c492895000242ad
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-26 12:40:49 -0300

    Refatora funçao de lista de skill e adiciona novas hard skills. Adiciona novos efeitos ao menu do header e flexibiliza melhor area de skill, tambem realizei alteraçoes no texto da bio, e alteraçoes no tamanho das fontes da para uma melhor leitura

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 const softSkillsList = document.querySelector('.my-soft-skills');
 const hardSkillsList = document.querySelector('.my-hard-skills')
-// Coloca uma soft skill para cada constante
-const softSkill1 = 'Disposição para aprender coisas novas';
-const softSkill2 = 'Bom em lidar com desafios;';
-const softSkill3 = 'Comunicativo;';
-const softSkill4 = 'Gosto de trabalhar em equipe';
-const softSkill5 = 'Comprometimento com as responsabilidades;';
-const softSkill6 = 'Tratar a todos com Empatia.';
 // Array contendo todas as soft skills
-const softSkills = [softSkill1 , softSkill2 , softSkill3 , softSkill4 , softSkill5, softSkill6];
-// Coloca uma hard skill para cada constante
-const hardSkill1 = 'Unix;';
-const hardSkill2 = 'Git;';
-const hardSkill3 = 'GitHub;';
-const hardSkill4 = 'HTML;';
-const hardSkill5 = 'CSS;';
-const hardSkill6 = 'Semantic HTML;';
-const hardSkill7 = 'JavaScript;';
-const hardSkill8 = 'DOM;';
-const hardSkill9 = 'Forms.';
+const softSkills = [
+    'Disposição para aprender coisas novas',
+    'Bom em lidar com desafios;',
+    'Comunicativo;',
+    'Gosto de trabalhar em equipe',
+    'Comprometimento com as responsabilidades;',
+    'Tratar a todos com Empatia.'
+];
 // Array contendo todas as hard skills
-const hardSkills = [hardSkill1 , hardSkill2 , hardSkill3 , hardSkill4 , hardSkill5, hardSkill6 , hardSkill7 , hardSkill8, hardSkill9];
-// Função que gera listas de soft skills dinâmicamente
+const hardSkills = [
+    'Unix;',
+    'Git;',
+    'HTML;',
+    'Semantic HTML;',
+    'CSS;',
+    'CSS Responsivo;',
+    'JavaScript;',
+    'ES6;',
+    'DOM;',
+    'Forms;',
+    'Unit Tests;',
+    'HOF.'
+];
+// Função que gera listas de skills dinâmicamente
 function createSkillsList(arrSkills, element) {
-    for (let index = 0; index < arrSkills.length; index += 1) {
-        const skill = arrSkills[index];
+    for (const index of arrSkills) {
+        const skill = index;
         const itemTag = document.createElement('li');
         itemTag.innerHTML = skill;
         element.appendChild(itemTag);
